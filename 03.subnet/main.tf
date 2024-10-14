4094c43af488082c2e36e2df52afa26ac84644cb
Nicholas Lawrence <nlawren@gmail.com>
2024-10-14 20:21:50 +1100

    Module 3 - creating a public subnet

---
+++ 03.subnet/main.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "mtc_public_subnet" {
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "dev-public"
+  }
+
+}
