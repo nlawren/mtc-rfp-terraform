8f92134b9a169a8865ff4c996245e85d08aadb62
Nicholas Lawrence <nlawren@gmail.com>
2024-10-17 16:27:50 +1100

    Module 5 Completed.

--- 05.route-table/main.tf
+++ 05.route-table/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "mtc_vpc" {
+resource "aws_vpc" "rfp_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -12,29 +12,29 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_subnet" "mtc_public_subnet" {
-  vpc_id                  = aws_vpc.mtc_vpc.id
+resource "aws_subnet" "rfp_public_subnet" {
+  vpc_id                  = aws_vpc.rfp_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
 
   tags = {
-    Name = "dev.public-subnet"
+    Name = "dev.public_subnet"
   }
 }
 
-resource "aws_internet_gateway" "mtc_internet_gateway" {
-  vpc_id = aws_vpc.mtc_vpc.id
+resource "aws_internet_gateway" "rfp_internet_gateway" {
+  vpc_id = aws_vpc.rfp_vpc.id
 
   tags = {
     Name = "dev.igw"
   }
 }
 
-resource "aws_route_table" "mtc_public_rt" {
-  vpc_id = aws_vpc.mtc_vpc.id
+resource "aws_route_table" "rfp_public_rt" {
+  vpc_id = aws_vpc.rfp_vpc.id
 
   tags = {
-    Name = "dev.public-rt"
+    Name = "dev.public_rt"
   }
 }
