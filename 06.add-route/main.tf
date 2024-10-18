21167f3bf3ba2ff6c6b2c40bb98a075108dbb23b
Nicholas Lawrence <nlawren@gmail.com>
2024-10-18 20:24:28 +1100

    Module 6 - adding the default route to the table.

--- 06.add-route/main.tf
+++ 06.add-route/main.tf
@@ -38,3 +38,9 @@ resource "aws_route_table" "rfp_public_rt" {
     Name = "dev.public_rt"
   }
 }
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.rfp_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.rfp_internet_gateway.id
+}
