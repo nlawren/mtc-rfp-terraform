745f85e141f41b0febe01811b409f0a8261c44b4
Nicholas Lawrence <nlawren@gmail.com>
2024-10-19 19:31:03 +1100

    General - added terraform version constraint

--- 02.vpc/providers.tf
+++ 02.vpc/providers.tf
@@ -1,10 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+  required_version = ">= 1.9"
 }
 
 provider "aws" {
