5ae44e24ae3d47835f97111c67fbb1257711df0e
Nicholas Lawrence <nlawren@gmail.com>
2024-10-20 12:41:04 +1100

    Module 8 Removed ingress rules as will be using tailscale.

--- 08.security-group/main.tf
+++ 08.security-group/main.tf
@@ -54,12 +54,6 @@ resource "aws_security_group" "rfp_sg" {
   name        = "public_sg"
   description = "Public security group"
   vpc_id      = aws_vpc.rfp_vpc.id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
