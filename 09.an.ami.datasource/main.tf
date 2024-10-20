aaa11287fd7b402722fa2ada32f9374e9d9e93e8
Nicholas Lawrence <nlawren@gmail.com>
2024-10-20 14:09:17 +1100

    Module 9 Using the ami datasource Initial test trying an Amazon Linux ami filter. Worked using "al2023-ami-2023.6.2024*-kernel-6.1-x86_64".

---
+++ 09.an.ami.datasource/main.tf
@@ -0,0 +1,60 @@
+resource "aws_vpc" "rfp_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev.vpc"
+  }
+}
+
+resource "aws_subnet" "rfp_public_subnet" {
+  vpc_id                  = aws_vpc.rfp_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "dev.public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "rfp_internet_gateway" {
+  vpc_id = aws_vpc.rfp_vpc.id
+
+  tags = {
+    Name = "dev.igw"
+  }
+}
+
+resource "aws_route_table" "rfp_public_rt" {
+  vpc_id = aws_vpc.rfp_vpc.id
+
+  tags = {
+    Name = "dev.public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.rfp_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.rfp_internet_gateway.id
+}
+
+resource "aws_route_table_association" "rfp_public_rtassoc" {
+  subnet_id      = aws_subnet.rfp_public_subnet.id
+  route_table_id = aws_route_table.rfp_public_rt.id
+}
+
+resource "aws_security_group" "rfp_sg" {
+  name        = "public_sg"
+  description = "Public security group"
+  vpc_id      = aws_vpc.rfp_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
